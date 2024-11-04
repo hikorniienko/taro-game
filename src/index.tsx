ca28999c6b7429922aa0de7e22728c2fd61e31fb
v.korniienko <v.korniienko@twinwingames.com>
2024-11-04 20:46:23 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Client } from 'appwrite';
+
+import App from './App';
+
+const client = new Client();
+client.setProject('6728ee60001d622e9a7a');
+
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
